05bf7f26a5b8d502e464813f782e198290553c53
Elias Kirui <eliaskips1@gmail.com>
2022-04-05 01:27:09 +0300

    Add stimulus auto submit

---
+++ app/models/task.rb
@@ -0,0 +1,6 @@
+class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { in: 5..20 }
+  validates :body, presence: true
+  validates :body, length: { in: 10..50 }
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :tasks do
+    collection do
+      post :preview
+    end
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "tasks#index"
+end
